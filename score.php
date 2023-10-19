d9d96eb2a8be5c39082a84a2cd87b259a39a4896
linuxadm.c <linuxadm.c@gmail.com>
2023-10-19 00:39:22 +0000

    rename

--- score.php
+++ score.php
@@ -1,25 +1,46 @@
-<?php
-$conn = new mysqli("localhost", "root", "Linux.adm@1", "new_database");
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Scores</title>
+</head>
+<body>
+    <h1>Student Scores</h1>
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Student Name</th>
+            <th>Score</th>
+            <th>Subject</th>
+        </tr>
 
-$sql = "SELECT id, student_name, score FROM scores";
-$result = $conn->query($sql);
+        <?php
+        $conn = new mysqli("localhost", "root", "Linux.adm@1", "new_database");
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row["id"] . "</td>";
-        echo "<td>" . $row["student_name"] . "</td>";
-        echo "<td>" . $row["score"] . "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "<tr><td colspan='3'>No records found</td></tr>";
-}
+        // Prepare and execute an SQL query to retrieve student scores
+        $sql = "SELECT id, student_name, score, subject FROM scores";
+        $result = $conn->query($sql);
 
-$conn->close();
-?>
+        if ($result->num_rows > 0) {
+            // Output data from each row
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["id"] . "</td>";
+                echo "<td>" . $row["student_name"] . "</td>";
+                echo "<td>" . $row["score"] . "</td>";
+                echo "<td>" . $row["subject"] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='3'>No records found</td></tr>";
+        }
 
+        // Close the database connection
+        $conn->close();
+        ?>
+    </table>
+</body>
+</html>
